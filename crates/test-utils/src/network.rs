01cd5f3ca278fb1c15c62038c8cbb23cda9b8b0b
patrick <patrickkuo@me.com>
2022-07-22 11:57:46 +0100

    more gateway deps clean up

--- crates/test-utils/src/network.rs
+++ crates/test-utils/src/network.rs
@@ -11,11 +11,12 @@ use sui::{
     client_commands::{SuiClientCommands, WalletContext},
     config::SuiClientConfig,
 };
+use sui_config::gateway::GatewayConfig;
 use sui_config::genesis_config::GenesisConfig;
 use sui_config::{Config, SUI_CLIENT_CONFIG, SUI_GATEWAY_CONFIG, SUI_NETWORK_CONFIG};
 use sui_config::{PersistedConfig, SUI_KEYSTORE_FILENAME};
 use sui_core::gateway_state::GatewayClient;
-use sui_gateway::config::{GatewayConfig, GatewayType};
+use sui_core::gateway_state::GatewayState;
 use sui_gateway::rpc_gateway_client::RpcGatewayClient;
 use sui_json_rpc::api::RpcGatewayApiServer;
 use sui_json_rpc::api::RpcReadApiServer;
@@ -25,6 +26,7 @@ use sui_json_rpc::gateway_api::{
     GatewayReadApiImpl, GatewayWalletSyncApiImpl, RpcGatewayImpl, TransactionBuilderImpl,
 };
 use sui_sdk::crypto::{KeystoreType, SuiKeystore};
+use sui_sdk::ClientType;
 use sui_swarm::memory::{Swarm, SwarmBuilder};
 use sui_types::base_types::SuiAddress;
 use sui_types::crypto::KeypairTraits;
@@ -86,7 +88,7 @@ pub async fn start_test_network_with_fullnodes(
     SuiClientConfig {
         accounts,
         keystore: KeystoreType::File(keystore_path),
-        gateway: GatewayType::Embedded(GatewayConfig {
+        gateway: ClientType::Embedded(GatewayConfig {
             db_folder_path,
             validator_set: validators,
             ..Default::default()
@@ -122,10 +124,9 @@ async fn start_rpc_gateway(
 ) -> Result<(SocketAddr, HttpServerHandle), anyhow::Error> {
     let server = HttpServerBuilder::default().build("127.0.0.1:0").await?;
     let addr = server.local_addr()?;
-    let registry = prometheus::Registry::new();
 
-    let config: GatewayConfig = PersistedConfig::read(config_path)?;
-    let client = config.create_client(&registry)?;
+    let config = PersistedConfig::read(config_path)?;
+    let client = GatewayState::create_client(&config, None)?;
     let mut module = RpcModule::new(());
     module.merge(RpcGatewayImpl::new(client.clone()).into_rpc())?;
     module.merge(GatewayReadApiImpl::new(client.clone()).into_rpc())?;
@@ -154,7 +155,7 @@ pub async fn start_rpc_test_network_with_fullnode(
         PersistedConfig::read(&working_dir.join(SUI_CLIENT_CONFIG))?;
     let rpc_url = format!("http://{}", server_addr);
     let accounts = wallet_conf.accounts.clone();
-    wallet_conf.gateway = GatewayType::RPC(rpc_url.clone());
+    wallet_conf.gateway = ClientType::RPC(rpc_url.clone());
     wallet_conf
         .persisted(&working_dir.join(SUI_CLIENT_CONFIG))
         .save()?;
